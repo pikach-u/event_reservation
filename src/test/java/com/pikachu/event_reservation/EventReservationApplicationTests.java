550a736854ac8ad78f7248f2e4bb662af8769201
pikach-u <ziwon05@naver.com>
2025-07-14 15:42:50 +0900

    first commit

---
+++ src/test/java/com/pikachu/event_reservation/EventReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pikachu.event_reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
