fe1af01c773634449e3a477380909b3cf8e35456
pikach-u <ziwon05@naver.com>
2025-07-16 10:43:08 +0900

    Event 추가 완료

--- src/main/java/com/pikachu/event_reservation/controller/EventController.java
+++ src/main/java/com/pikachu/event_reservation/controller/EventController.java
@@ -1,7 +1,12 @@
 package com.pikachu.event_reservation.controller;
 
+import com.pikachu.event_reservation.dto.EventDto;
+import com.pikachu.event_reservation.model.Event;
 import com.pikachu.event_reservation.service.EventService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class EventController {
     private final EventService eventService;
+
+    @PostMapping
+    public Event create(@Valid @RequestBody EventDto eventDto) {
+        Event event = new Event();
+        event.setName(eventDto.getName());
+        event.setEventData(eventDto.getEventDate());
+        event.setLocation(eventDto.getLocation());
+
+        return eventService.create(event);
+    }
 }
